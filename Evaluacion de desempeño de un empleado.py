d6dba51870586e58f2c943f7dbf0d49c908db513
joselopezx <jose0xlopez@gmail.com>
2025-11-27 22:31:08 -0600

    Cambio a la carpeta Practicando IF, elif y else

--- Evaluacion de desempeño de un empleado.py
+++
@@ -1,8 +0,0 @@
-puntuacionDesempenio = int(input("Ingrese el desempeño del empleado (escala de 0 - 10)"))
-aniosTrabajados = int(input("Ingrese los años trabajados del empleado"))
-if puntuacionDesempenio<7:
-    print("Necesita mejorar")
-elif aniosTrabajados<=5:
-    print("Necesita mejorar")
-else:
-    print("Elegible para ascenso")
