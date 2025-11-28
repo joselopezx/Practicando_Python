754ffff67c2d33078d1676859598b8b6a1bcfa65
joselopezx <jose0xlopez@gmail.com>
2025-11-27 22:32:55 -0600

    Cambio a la carpeta Practicando IF, elif y else

--- Verificando acceso a beneficios sociales.py
+++
@@ -1,7 +0,0 @@
-ingresos = float(input("Ingresos percibidos : "))
-cantidadHijos = int(input("Cuantos hijos tiene? "))
-
-if ingresos<=2000 and cantidadHijos>=1:
-    print("Tiene derecho a beneficio")
-else:
-    print("No tiene derecho a beneficio")
